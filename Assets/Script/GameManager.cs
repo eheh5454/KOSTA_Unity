3503498950cc17a2380b216f4afd1d24bf7aa074
eheh5454 <eheh5454@gmail.com>
2020-02-25 12:01:35 +0900

    IP PORT 입력받아 라지그 키트 이동 제어

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //안드로이드 기준 뒤로가기 입력 시 
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/Script/Move.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine.UI;
+using TMPro;
+
+public enum DIR
+{
+    go,
+    back,
+    right,
+    left,
+    close
+};
+
+public class Move : MonoBehaviour
+{
+    public DIR dir;    
+    public TMP_InputField IP_field;
+    public TMP_InputField PORT_field;    
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IP_field = GameObject.Find("Field_IP").GetComponent<TMP_InputField>();        
+        PORT_field = GameObject.Find("Field_PORT").GetComponent<TMP_InputField>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Send_Move()
+    {
+        Debug.Log(IP_field.text);
+
+        Debug.Log(PORT_field.text);
+
+        Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        EndPoint ep = new IPEndPoint(IPAddress.Parse(IP_field.text.ToString()), int.Parse(PORT_field.text));
+
+        sock.SendTo(Encoding.Default.GetBytes(dir.ToString()), ep);
+    }
+}
